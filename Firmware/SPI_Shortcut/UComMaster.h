4b6992fca8e2b68348b69fc08b96be95a38640f7
Toni Klopfenstein <toni.klopfenstein@sparkfun.com>
2013-08-22 10:42:04 -0600

    Initial commit of v16 files

---
+++ Firmware/SPI_Shortcut/UComMaster.h
@@ -0,0 +1,136 @@
+//*******************************************************
+//
+//		   SPI Shortcut Pin Definitions 
+//				SparkFun Electronics
+//   		   Written By Chris Taylor
+//			
+//*******************************************************
+//HARDWARE NOTE: High Fuse should be set to 0xDF
+//				 Low Fuse should be set to 0xFF
+
+//*******************************************************
+//					General Definitions
+//*******************************************************
+#define HOLD_TIME	20
+#define OK	1
+#define ERROR	0
+
+#define CS		PB2
+#define MOSI	PB3
+#define MISO 	PB4
+#define SCK		PB5
+
+#define LED		PB1
+
+#define SCL 	PC5
+#define SDA		PC4
+
+
+//*******************************************************
+//					ATmega168 Pin Definitions
+//*******************************************************
+
+#define sbi(var, mask)   ((var) |= (uint8_t)(1 << mask))
+#define cbi(var, mask)   ((var) &= (uint8_t)~(1 << mask))
+
+//SPI Communication Pins
+#define SPI_SCLK	5
+#define SPI_IN		4
+#define SPI_OUT		3
+#define SPI_CS		2
+
+//FT245 Control Pin Definitions
+#define PWREN	(1<<0) //PB0 (Input to AVR)
+#define RD		(1<<0) //PC0 (Output from AVR)
+#define WR		(1<<1) //PC1 (Output from AVR)
+#define TXE		(1<<2) //PC2 (Input to AVR)
+#define RXF		(1<<3) //PC3 (Input to AVR)
+
+//FT245 Data Pins (These pins switch between being I/O)
+#define D0		(1<<0) //PD0
+#define D1		(1<<1) //PD1
+#define D2		(1<<2) //PD2
+#define D3		(1<<3) //PD3
+#define D4		(1<<4) //PD4
+#define D5		(1<<5) //PD5
+#define D6		(1<<6) //PD6
+#define D7		(1<<7) //PD7
+
+//*******************************************************
+//					Global Macros
+//*******************************************************
+#define sbi(var, mask)   ((var) |= (uint8_t)(1 << mask))
+#define cbi(var, mask)   ((var) &= (uint8_t)~(1 << mask))
+
+//Macros for interfacing with the FT245
+#define USBreadAllowed()	(PINC & RXF) != RXF
+#define USBwriteAllowed()  	(PINC & TXE) != TXE
+#define USBConfigured() 	(PINB & PWREN) != PWREN
+#define setRD()		PORTC |= RD;
+#define clearRD()	PORTC &= ~RD;
+#define setWR()		PORTC |= WR;
+#define clearWR()	PORTC &= ~WR;
+
+//*******************************************************
+//					Global Functions
+//*******************************************************
+void delay_ms(uint16_t x); // general purpose delay
+void delay_micro(uint16_t x); // general purpose delay
+void fill16(volatile uint8_t* src, uint8_t fill);
+void fillFrame(volatile uint8_t* src, uint8_t fill);
+void newcolor(uint8_t * r, uint8_t * g, uint8_t * b);
+void clock_byte(volatile uint16_t my_byte, volatile uint8_t rowNum);
+void ioinit (void);      // initializes IO
+void setDataOutputs(void);
+void setDataInputs(void);
+void printf245(const char *text, ...);
+char getchar245(void);
+void reverse(char s[]);
+void itoa2(int n, char s[]);
+void parseCommand(void);
+void executeCommand(void);
+char string_compare(const char *search_string, const char *find_string);
+void ascii2hex(volatile char *ascii_string);
+void setPixel(int board, int offset, char r, char g, char b);
+void setBoard(char r, char g, char b);
+void sendButtonValues(void);
+unsigned char read_from_EEPROM(unsigned int Address);
+void write_to_EEPROM(unsigned int Address, unsigned char Data);
+
+//*******************************************************
+//					Structure Definitions
+//*******************************************************
+typedef struct{
+	char ID;
+	char Parameters[14];
+	//char Checksum;
+} Packet;
+
+//*******************************************************
+//					my_printf definitions 
+//*******************************************************
+#define MAIN_MENU 0
+#define ARROW 1
+#define INVALID 2
+#define SEND_SINGLE 3 
+#define ACTIONS_MENU 4
+#define CONT_RECEIVE 5
+#define SEND_COMMAND 6
+#define CS_HIGH 7
+#define CS_LOW 8 
+#define STRING_SENT 9
+#define SETTINGS 10
+#define SET_POLARITY_1 11
+#define SET_POLARITY_2 12
+#define SET_POLARITY_3 13
+#define MODE_CHANGED 14
+#define FREQUENCY_MENU_1 15
+#define FREQUENCY_MENU_2 16
+#define FREQUENCY_CHANGED 17
+#define DORDER_MENU 18
+#define DORDER_CHANGED 19
+#define CURRENT_SETTINGS 20
+#define DATA_ORDER 21
+#define MSB 22
+#define LSB 23
+#define DELAY 24
---
+++ Firmware/SPI_Shortcut/ft245.c
@@ -0,0 +1,231 @@
+#include <inttypes.h>
+#include <ctype.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include "UComMaster.h"
+
+//Initializes the I/O pins, the interrupts and the system parameters
+void ioinit (void)
+{
+    //1 = output, 0 = input   
+	DDRC |= WR | RD;
+	DDRC &= ~(RXF | TXE);
+	DDRB &= ~(PWREN);
+	PORTC |= (RXF | TXE);
+	PORTB |= (PWREN);
+	
+	DDRD &= ~(D0 | D1 | D2 | D3 | D4 | D5 | D6 | D7);
+	PORTD |= (D0 | D1 | D2 | D3 | D4 | D5 | D6 | D7);
+	
+}   
+
+void delay_ms(uint16_t x)
+{
+  uint8_t y, z;
+  for ( ; x > 0 ; x--){
+    for ( y = 0;  y < 40 ; y++){
+      for ( z = 0 ; z < 40 ; z++){
+        asm volatile ("nop");
+      }
+    }
+  }
+}
+
+//General short delays
+void delay_micro(uint16_t x)
+{
+  uint8_t y;
+  for ( ; x > 0 ; x--){
+    for ( y = 0;  y < 4 ; y++){
+		asm volatile ("nop");
+    }
+  }
+}
+
+//Configures the I/O pins to output to the FT245
+void setDataOutputs(void){
+    //1 = output, 0 = input
+	PORTD &= ~(D0 | D1 | D2 | D3 | D4 | D5 | D6 | D7); //Disable Pullups (Intermediate Step)
+	DDRD |= (D0 | D1 | D2 | D3 | D4 | D5 | D6 | D7);
+}
+
+//Configures the I/O pins to input from the FT245
+void setDataInputs(void){
+    //1 = output, 0 = input	
+	DDRD &= ~(D0 | D1 | D2 | D3 | D4 | D5 | D6 | D7);
+	PORTD |= (D0 | D1 | D2 | D3 | D4 | D5 | D6 | D7); //Enable Pullups
+}
+
+//Printf a string to the FT245. Works like printf
+void printf245(const char *text, ...){
+	char character=0, temp=0;
+	char decimal[6];
+	int index=0;
+	
+	va_list next_argument;		//Create a pointer for the variable argument
+	va_start(next_argument, text);	//Initialize the pointer to the next argument in the list
+	
+	setDataOutputs();
+	character=text[index];
+	while(character!='\0'){
+		if(USBwriteAllowed()){
+			if(character=='%'){	//Need to access an argument value!
+				character = (char)va_arg(next_argument, int);	//Get the value of the current argument
+				
+				if(text[index+1]=='d'){
+					itoa2(character, decimal);
+					for(int i=0; decimal[i]!='\0'; i++)printf245("%c", decimal[i]);
+					index+=1;
+				}
+				else if(text[index+1]=='x'){
+					temp=character;
+					temp=temp>>4;
+					if(temp>9)temp+=('A'-10);
+					else temp+='0';
+					printf245("%c", temp);
+					character=character&0x0f;
+					if(character>9)character+=('A'-10);
+					else character+='0';
+					printf245("%c", character);
+					index+=1;
+				}
+				else if(text[index+1]=='c'){
+					PORTD = character;				
+					clearWR();
+					delay_ms(1);
+					setWR();
+					index+=1;
+				}				
+			}
+			else{
+				PORTD = character;				
+				clearWR();
+				delay_ms(1);
+				setWR();
+			}
+			character=text[++index];
+		}
+	}
+	va_end(next_argument);	//Relinquish the pointer to system memory
+}
+
+//Returns a character from the FT245
+char getchar245(void){
+	char character=0;
+	
+	setDataInputs();
+	if(USBreadAllowed()){		//If RX Flag is low, lets get the data from the FIFO
+		clearRD();			//Clock the next FIFO data onto the D0-D7 lines
+		delay_micro(HOLD_TIME);
+		character = PIND;		//Get data from FT245		
+		setRD();			//Relinquish control of the Data lines
+	}
+	
+	return character;
+}
+
+/* reverse:  reverse string s in place */
+void reverse(char s[])
+{
+    int c, i, j;
+
+    for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+/* itoa:  convert n to characters in s */
+void itoa2(int n, char s[])
+{
+    int i=0;
+
+    do {       /* generate digits in reverse order */
+        s[i++] = n % 10 + '0';   /* get next digit */
+    } while ((n /= 10) > 0);     /* delete it */
+
+    s[i] = '\0';
+    reverse(s);
+}
+
+
+
+//Use to find a string within a search string
+//search : "hithere\0"
+//find : "the\0" - return OK
+//***************************** UNTESTED *********************************
+char string_compare(const char *search_string, const char *find_string)
+{
+
+    int find_spot, search_spot;
+    char spot_character, search_character;
+    find_spot = 0;
+
+    for(search_spot = 0 ; ; search_spot++)
+    {
+        if(find_string[find_spot] == '\0') return OK; //We've reached the end of the search string - that's good!
+        if(search_string[search_spot] == '\0') return ERROR; //End of string found
+
+        spot_character = find_string[find_spot]; //Compiler limit
+        search_character = search_string[search_spot]; //Compiler limit
+
+        if(spot_character == search_character) //We found another character
+            find_spot++; //Look for the next spot in the search string
+        else if(find_spot > 0) //No character found, so reset the find_spot
+            find_spot = 0;
+    }
+
+    return 0;
+}
+
+
+
+
+void ascii2hex(volatile char *ascii_string)
+{
+	char hex_value;
+	char hex_string[20];
+	int hex_size=0;
+	int ascii_index=1;
+	
+	//Convert each character in the received command buffer to a hex value
+	for(int string_index=1; ascii_string[string_index] != '\n'; string_index++){
+		hex_value=ascii_string[string_index];
+		if(isdigit(hex_value))hex_value-='0';
+		else if(isupper(hex_value)){
+			hex_value-='A';
+			hex_value+=10;
+		}
+		else if(islower(hex_value)){
+			hex_value-='a';
+			hex_value+=10;
+		}
+		hex_string[string_index-1]=hex_value;
+		hex_size++;
+	}
+	hex_string[hex_size]='\n';
+
+	//Now that we have the hex values, we need to concatonate each "pair" since we received the command serially.
+	//(i.e. a received command: "01A23E" is 6 characters, but the hex command is only 3 characters)
+	for(int index=0; hex_string[index]!='\n'; index++){
+		
+		//Combine two values in the hex string to form a single hex byte
+		hex_value=((hex_string[index]&0x0f)<<4) | (hex_string[index+1]&0x0f);
+		
+		//Put the new hex value back in the ascii string for use by the main program
+		ascii_string[ascii_index++]=hex_value;
+		
+		//Increment the hex index since we are only getting "pairs"
+		index+=1;
+		hex_size=index;
+	}
+	ascii_index=1;
+	ascii_string[hex_size/2+2]='\n';
+	
+}
+
+
